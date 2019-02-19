9841cafc77aaad89360dc8c645771b80cd3e188e
Shaan Khan <41818155+skhan117@users.noreply.github.com>
2019-02-19 00:42:18 -0800

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,6 +1,15 @@
 /*
-    A Board object has a two-dimensional integer array
-    depicting a particular state of the chess board where:
+    Board class models a chessboard, the positions of its pieces, and the
+    possible moves its pieces may make. 
+*/
+
+import java.util.*;
+
+public class Board {
+
+    /*
+    A two-dimensional integer array boardState
+    depicts a particular state of the chess board where:
     -white pawns are represented by 1.
     -white bishops are represented by 2.
     -white knights are represented by 3.
@@ -9,15 +18,10 @@
     -the white king is represented by 6.
     -black pieces are represented by -n. e.g a black pawn
      is -1, a black bishop is -2, etc.
-
-*/
-
-import java.util.*;
-
-public class Board {
-    
+    */
     private int [][] boardState;   
-    private ArrayList <Integer> deadPieces;
+    
+    private ArrayList <Integer> deadPieces; 
     
     // Default constructor starts a new board.
     public Board() {
@@ -60,8 +64,7 @@ public class Board {
         boardState[0][2] = -2;
         boardState[0][5] = -2;
         boardState[0][3] = -5;
-        boardState[0][4] = -6;
-        
+        boardState[0][4] = -6;        
     }
 
     // makeAWhitePawnMove takes four arguments:
@@ -75,7 +78,9 @@ public class Board {
     // to ArrayList deadPieces.
     public Boolean makeAWhitePawnMove (int originalRow, int originalColumn, 
             int proposedRow, int proposedColumn) {
-                 
+        
+        // FIXME: First pawn move can be two spaces!
+        
         
         // Return false if pawn isn't trying to move one row ahead.
         if (!(proposedRow == originalRow - 1)) {
@@ -88,7 +93,6 @@ public class Board {
         if (boardState[proposedRow][proposedColumn] > 0) {
             System.out.println("Out 2"); // FIXME
             return false;
-
         }
         
         // If there's a black piece in proposed place, move must be diagonal.
@@ -105,7 +109,6 @@ public class Board {
                 return false;
             }
         }
-
 
         // If the proposed position is empty and 1 row ahead of pawn, return
         // true and make the move
@@ -148,9 +151,121 @@ public class Board {
     // to ArrayList deadPieces.
     public Boolean makeAWhiteRookMove (int originalRow, int originalColumn, 
             int proposedRow, int proposedColumn) {
-                 
+                
+        // if rook isn't where command says it should be, return false
+        if (boardState[originalRow][originalColumn] != 4) {
+            System.out.println("\nRook isn't there\n");
+            return false;
+        }
         
+        int checkRow = originalRow;
+        int checkColumn = originalColumn;
         
+        // Make an 8x8 2D array
+        boolean [][] validRookMoves = new boolean[8][8];
+        
+        // Check up from rook
+        while (checkRow != 0) {
+            checkRow--;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        // Check down from rook
+        checkRow = originalRow;
+        checkColumn = originalColumn;
+        while (checkRow != 7) {
+            checkRow++;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        // Check left from rook
+        checkRow = originalRow;
+        checkColumn = originalColumn;
+        while (checkColumn != 0) {
+            checkColumn--;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        // Check right from rook
+        checkRow = originalRow;
+        checkColumn = originalColumn;
+        while (checkColumn != 7) {
+            checkColumn++;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validRookMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        /* For testing: Print potential rook moves
+        System.out.println();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (validRookMoves[i][j] == true) {
+                    System.out.print(validRookMoves[i][j] + "  ");
+                }
+                else {
+                    System.out.print(validRookMoves[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        */
+        
+        // if proposed move is not a valid rook move, return false
+        if (validRookMoves[proposedRow][proposedColumn] == false) {
+            System.out.println();
+            System.out.println("Not a valid rook move\n");
+            return false;
+        }
+        
+        // If rook's proposed position is empty, move it there
+        if (boardState[proposedRow][proposedColumn] == 0) {
+            boardState[originalRow][originalColumn] = 0;
+            boardState[proposedRow][proposedColumn] = 4;
+        }
+        // If rook's proposed position is occupied by opponent, take its place
+        // and add defeated foe to deadPieces Array List
+        else if (boardState[proposedRow][proposedColumn] < 0) {
+            deadPieces.add(boardState[proposedRow][proposedColumn]);
+            boardState[originalRow][originalColumn] = 0;
+            boardState[proposedRow][proposedColumn] = 4;
+        }
+        
+        System.out.println("\nRook's move was successful\n");
         return true;
         }
 
@@ -168,6 +283,8 @@ public class Board {
                  
         
         
+        
+        
         return true;
         }
 
@@ -183,8 +300,123 @@ public class Board {
     public Boolean makeAWhiteBishopMove (int originalRow, int originalColumn, 
             int proposedRow, int proposedColumn) {
                  
+        // if bishop isn't where command says it should be, return false
+        if (boardState[originalRow][originalColumn] != 2) {
+            System.out.println("\nBishop isn't there\n");
+            return false;
+        }
         
+        int checkRow = originalRow;
+        int checkColumn = originalColumn;
         
+        // Make an 8x8 2D array
+        boolean [][] validBishopMoves = new boolean[8][8];
+        
+        // Check up-right from bishop
+        while ((checkRow != 0) && (checkColumn != 7)) {
+            checkRow--;
+            checkColumn++;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+                
+        // Check down-right from bishop
+        checkRow = originalRow;
+        checkColumn = originalColumn;
+        while ((checkRow != 7) && (checkColumn != 7)) {
+            checkRow++;
+            checkColumn++;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        // Check down-left from bishop
+        checkRow = originalRow;
+        checkColumn = originalColumn;
+        while ((checkRow != 7) && (checkColumn != 0)) {
+            checkRow++;
+            checkColumn--;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        // Check up-left from bishop
+        checkRow = originalRow;
+        checkColumn = originalColumn;
+        while ((checkRow != 0) && (checkColumn != 0)) {
+            checkRow--;
+            checkColumn--;
+            if (boardState[checkRow][checkColumn] == 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+            }
+            else if (boardState[checkRow][checkColumn] > 0) {
+                break;
+            }
+            else if (boardState[checkRow][checkColumn] < 0) {
+                validBishopMoves[checkRow][checkColumn] = true;
+                break;
+            }
+        }
+        
+        // For testing: Print potential bishop moves
+        System.out.println("Bishop moves");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (validBishopMoves[i][j] == true) {
+                    System.out.print(validBishopMoves[i][j] + "  ");
+                }
+                else {
+                    System.out.print(validBishopMoves[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+        
+        // if proposed move is not a valid rook move, return false
+        if (validBishopMoves[proposedRow][proposedColumn] == false) {
+            System.out.println();
+            System.out.println("Not a valid rook move\n");
+            return false;
+        }
+        
+        // If bishop's proposed position is empty, move it there
+        if (boardState[proposedRow][proposedColumn] == 0) {
+            boardState[originalRow][originalColumn] = 0;
+            boardState[proposedRow][proposedColumn] = 2;
+        }
+        // If rook's proposed position is occupied by opponent, take its place
+        // and add defeated foe to deadPieces Array List
+        else if (boardState[proposedRow][proposedColumn] < 0) {
+            deadPieces.add(boardState[proposedRow][proposedColumn]);
+            boardState[originalRow][originalColumn] = 0;
+            boardState[proposedRow][proposedColumn] = 2;
+        }
+
+        System.out.println("Bishop move was successful");
         return true;
         }
 
@@ -228,6 +460,7 @@ public class Board {
     
     // Print board method
     public void printBoard() {
+        System.out.println("");
         for (int i = 0; i < 8; i++) {
 
             for (int j = 0; j < 8; j++) {
@@ -238,10 +471,8 @@ public class Board {
                 else {
                     System.out.print(boardState[i][j] + " ");
                 }
-                
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
-
